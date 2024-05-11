bc3dd4606ff57e0c5d4d5244f791d7e0339068cc
codzk <zekemilne@gmail.com>
2024-05-11 11:16:24 +1000

    feat: child routes working

--- src/App_ObjectRouting.jsx
+++ src/App_ObjectRouting.jsx
@@ -1,32 +1,39 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
+import BaseLayout from './pages/_baseLayout'
+
+
+const someRouterBlahBlah = createBrowserRouter([
+  {
+    path:"/",
+    element: <BaseLayout />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "about",
+        element: <AboutPage />
+      }, 
+      {
+        path: "contact",
+        element: <ContactPage />
+      }
+    ]
+  }
+]);
 
 function App() {
-  
-
   return (
     <>
-    <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/about/services' element={<AboutPage />} />
-          <Route path='/contact' element={<ContactPage />} />
-          {/* Need a page for any not-matched route */}
-          <Route path='*' element={<PageNotFound />} /> 
-
-          {/* <Route path='/' element={<HomePage />}>
-            <Route path='about'>
-              <Route path='services' element={<AboutPage />} />
-            </Route>
-            <Route path='contact' element={<ContactPage />} />
-          </Route> */}
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={someRouterBlahBlah} />
     </>
   )
 }
